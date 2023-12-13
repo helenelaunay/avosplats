a0f2492b89a65c791f88ebef75760203f0b07b25
Helene L <133009367+helenelaunay@users.noreply.github.com>
2023-12-13 15:35:00 +0100

    Front Page Contact fait avec Responsive

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,37 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
-<div><h2>Une question ? Une suggestion ?</h2></div>
-<div><h4>Contactez-nous via ce formulaire :</h4></div>
-<p>ou par mail à l'adresse suivante : contact@avosplats.fr ou en cliquant <a href="mailto:contact@avosplats.fr">ici</a></p>
-    <div>
-        <form action="{{ route('submitFormContact') }}" method="POST">
-            @csrf
-            @method('POST')
-            <div class="form-group form-row">
-                <div class="col">
+    <section id="contact">
+        <div>
+            <h2>Une question ? Une suggestion ?</h2>
+        </div>
+        <div>
+            <h4>N'hésitez pas à nous contacter :
+                <ul>
+                    <li>par mail à l'adresse suivante contact@avosplats.fr ou en <a id="link-mailTo"
+                            class="text-decoration-none" href="mailto:contact@avosplats.fr">cliquant ici</a></li>
+                    <li>via le formulaire suivant</li>
+                </ul>
+            </h4>
+        </div>
+
+        <div id="contactForm" class="container">
+            <form action="{{ route('submitFormContact') }}" method="POST">
+                @csrf
+                @method('POST')
+                <div class="form-group mt-2">
                     <label for="nom">Nom</label>
                     <input type="text" class="form-control" name="nom" id="nom" placeholder="Nom">
                 </div>
-                <div class="col">
+                <div class="form-group mt-2">
                     <label for="prenom">Prénom</label>
                     <input type="text" class="form-control" name="prenom" id="prenom" placeholder="Prénom">
                 </div>
-            </div>
-            <div class="form-group">
-                <label for="email">E-mail</label>
-                <input type="email" class="form-control" name="email" id="email" placeholder="votreadressemail@exemple.fr">
-            </div>
-            <div class="form-group">
-                <label for="message">Votre message</label>
-                <textarea class="form-control" id="message" name="message" rows="5" placeholder="Ecrivez votre message ici"></textarea>
-            </div>
-            <div class="form-group form-check">
-                <input type="checkbox" class="form-check-input" id="acceptTerms" name="acceptTerms" onchange="enableSubmit()">
-                <label class="form-check-label" for="acceptTerms">J'accepte les termes et conditions</label>
-            </div>
-            
-            <button type="submit" id="submitBtn" disabled class="btn btn-primary">Envoyer</button>
-        </form>        
-    </div>
+
+                <div class="form-group mt-2">
+                    <label for="email">E-mail</label>
+                    <input type="email" class="form-control" name="email" id="email"
+                        placeholder="votreadressemail@exemple.fr">
+                </div>
+                <div class="form-group mt-2">
+                    <label for="message">Votre message</label>
+                    <textarea class="form-control" id="message" name="message" rows="5" placeholder="Ecrivez votre message ici"></textarea>
+                </div>
+                <div class="form-group form-check mt-2">
+                    <input type="checkbox" class="form-check-input" id="acceptTerms" name="acceptTerms"
+                        onchange="enableSubmit()">
+                    <label class="form-check-label" for="acceptTerms">J'accepte les termes et conditions</label>
+                </div>
+                <div class="text-right">
+                    <button class="mt-5" type="submit" id="submitBtn" disabled>Envoyer</button>
+                </div>
+            </form>
+        </div>
+    </section>
 @endsection
